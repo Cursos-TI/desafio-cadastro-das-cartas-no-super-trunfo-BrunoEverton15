e8fc817d07ce0657339faf51ef7fcdb3087c3850
BrunoEverton15 <brunoeverton790@gmail.com>
2025-02-19 00:40:33 +0000

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 
 int main() {     
-  
 
-    int carta;
-   char nome, estado;
-    float Codigo_da_Carta;
+     
+  int populacao, ponto_turistico;
+ char nome[500];
+ float area, pib;
+ char letra; 
+  int numero;
 
 
-    printf("Carta: \n");
-    scanf("%d", &carta);
+        printf("Digite Numero da Carta: \n");
+        scanf("%d", &numero);
+
+        printf("Digite Letra Da Carta: \n");
+        scanf("%s", &letra);
+
+        printf("Digite Nome da Cidade: \n");
+        scanf("%s", &nome);
+
+        printf("Quantidade de Habitantes: \n");
+        scanf("%d", &populacao);
+
+        printf("Informe a Expansao da Area: \n");
+        scanf("%f", &area);
+
+        printf("Me Informe o Pib: \n");
+        scanf("%f", &pib);
+
+        printf("Quantidade de Pontos Turisticos: \n");
+        scanf("%d", &ponto_turistico);
+
     
-     printf("Estado: \n");
-    scanf("%s", &estado);
-  
-    printf("Codigo da Carta: \n");
-    scanf("%s", &Codigo_da_Carta);
-    printf("Codigo da Carta %f\n", Codigo_da_Carta);
+        printf("Cidade: %s\n", nome);
+        printf("Populacao: %d\n", populacao);
+        printf("Area: %f\n", area);
+        printf("Pib: %f\n", pib);
+        printf("Ponto Turistico: %d\n", ponto_turistico);
 
+
+
+return 0;
 
   
 
