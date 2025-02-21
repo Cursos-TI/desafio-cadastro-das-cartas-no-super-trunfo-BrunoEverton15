735b50af36feea1c66787ffac0b17c4690ed74fd
BrunoEverton15 <brunoeverton790@gmail.com>
2025-02-21 01:25:41 +0000

    commit.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,66 +1,52 @@
-#include <stdio.h>
+#include <stdio.h>;
 
-int main() {     
+int main(){
 
-     
-  int populacao, ponto_turistico;
- char nome[500];
- float area, pib;
- char letra; 
-  int numero;
-
-
-        printf("Digite Numero da Carta: \n");
-        scanf("%d", &numero);
-
-        printf("Digite Letra Da Carta: \n");
-        scanf("%s", &letra);
-
-        printf("Digite Nome da Cidade: \n");
-        scanf("%s", &nome);
-
-        printf("Quantidade de Habitantes: \n");
-        scanf("%d", &populacao);
-
-        printf("Informe a Expansao da Area: \n");
-        scanf("%f", &area);
-
-        printf("Me Informe o Pib: \n");
-        scanf("%f", &pib);
-
-        printf("Quantidade de Pontos Turisticos: \n");
-        scanf("%d", &ponto_turistico);
-
-    
-        printf("Cidade: %s\n", nome);
-        printf("Populacao: %d\n", populacao);
-        printf("Area: %f\n", area);
-        printf("Pib: %f\n", pib);
-        printf("Ponto Turistico: %d\n", ponto_turistico);
-
-
-
-return 0;
-
-  
+    char estado [10];
+    char codigo [5];
+    char cidade [20];
+    int populacao;
+    int Pontosturistico;
+    float area;
+    float pib;
 
 
 
 
+    printf("Digite Seu Estado: \n");
+    scanf("%s", &estado);  
+
+    printf("Codigo: \n");
+    scanf("%s", &codigo);
+
+    printf("Nome Da Cidade: \n");
+    scanf("%s", &cidade); 
+
+    printf("Total de Habitantes: \n");
+    scanf("%d", &populacao);
+
+    printf("Area: \n");
+    scanf("%f", &area);
+
+    printf("Pib: \n");
+    scanf("%f", &pib);
+
+    printf("Quantidade de Pontos Turisticos: \n");
+    scanf("%d", &Pontosturistico);
+
+
+    printf("Carta 1! \n");
+    printf("Estado: %s \n", estado);
+    printf("Codigo da Carta: %s%s \n", estado, codigo);
+    printf("Cidade: %s \n", cidade);
+    printf("Habitantes: %d \n", populacao);
+    printf("Area: %f \n", area);
+    printf("Pib: %f \n", pib);
+    printf("Pontos Turisticos: %d \n", Pontosturistico);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 
 
 
